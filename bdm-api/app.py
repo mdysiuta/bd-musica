703f491fa47e16885553aafac6dbcb3d54b5793a
mdysiuta <mdysiuta@gmail.com>
2025-10-16 22:39:49 -0300

    Creación de tablas y registro de blueprint de artista

--- bdm-api/app.py
+++ bdm-api/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
-@app.route("/")
-def hello():
-    return "<h1>Hola, mundo</h1>"
+from models import db
+
+# Crear las tablas de la base de datos
+with app.app_context():
+    db.create_all()
+
+from blueprints import artists
+
+app.register_blueprint(artists)
