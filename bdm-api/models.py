8072cd3c4a561e8074daf8c2dfefef235d8f927a
mdysiuta <mdysiuta@gmail.com>
2025-10-17 17:45:41 -0300

    Añadida columna 'slug' en modelo de artista

--- bdm-api/models.py
+++ bdm-api/models.py
@@ -1,16 +1,19 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from app import app
+from util import create_slug
 
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+migrate = Migrate(app, db)
 
 class Artist(db.Model):
     __tablename__ = "artists"
     id:   Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
-    # slug: Mapped[str] = mapped_column(unique=True)
+    slug: Mapped[str] = mapped_column(unique=True)
