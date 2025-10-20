e122b0b5404428ecba0e0c26044865992db86a8d
mdysiuta <mdysiuta@gmail.com>
2025-10-20 16:07:31 -0300

    Limpiado el archivo util.py además de añadir funciones de filtrado

--- bdm-api/util.py
+++ bdm-api/util.py
@@ -1,61 +1,70 @@
 def create_slug(string : str) -> str :
+    """
+    Devuelve un slug creado a partir de un string para usar en URLs.
+    """
     newString = string
 
-    newString = newString.replace(" ", "-")
-    newString = newString.replace(".", "_")
-    newString = newString.replace("/", "_")
-    newString = newString.replace("'", "")
-    newString = newString.replace("\"", "")
-    newString = newString.replace("[", "")
-    newString = newString.replace("]", "")
+    from data import slugForbiddenSymbols
+
+    # Remover y reemplazar símbolos no permitidos en el slug
+    for symbol in slugForbiddenSymbols:
+        newString = newString.replace(
+            symbol.get("symbol"),
+            symbol.get("replace-with"))
+
     newString = newString.lower()
 
     return newString
 
-# Verificar si existe artista con el slug creado.
-# Si existe, incrementar el contador por uno.
-# Pegar el valor del contador al slug original para crear un nuevo slug.
-# Verificar si existe un artista con ese nuevo slug y si lo hay, repetir
-# el proceso hasta que la consulta no devuelva una fila.
 def generate_slug(string : str, model) -> str :
+    """
+    Generar un slug único para un dato. Un número se agrega a un slug generado
+    si se encuentran repeticiones dentro de una tabla.
+    """
     slug = create_slug(string)
 
     from models import db
 
+    # contador para datos encontrados en las consultas
+    resultsFound = 0
+
     query = db.select(model).filter_by(slug=slug)
     result = db.session.execute(query).scalar()
-
-    count = 0
 
     if (result == None):
         return slug
     else:
         while (result != None):
-            count += 1
-            newSlug = slug + "-" + str(count)
+            resultsFound += 1
+            newSlug = slug + "-" + str(resultsFound)
+
+            # verificar existencia de un dato que contiene el slug nuevo
             query = db.select(model).filter_by(slug=newSlug)
             result = db.session.execute(query).scalar()
+
             if (result == None):
                 return newSlug
 
-def get_genre_tree(genre):
-    return [{
-        "id": genre.id,
-        "name": genre.name,
-        "slug": genre.slug,
-        "subgenres": get_genre_tree(genre)
-    } for genre in genre.subgenres]
+def filter_artist_query(query, request):
+    """
+    Devuelve una consulta filtrada a partir de parametros entregados a través
+    de una URL.
+    """
+    if request.values.get("name") != None:
+        query = query.filter_by(name=request.values.get("name"))
 
-def create_genre_dict(genre):
-    return {
-        "id": genre.id,
-        "name": genre.name,
-        "top":  genre.top,
-        "slug": genre.slug,
-        "parents": [{
-            "id": genre.id,
-            "name": genre.name,
-            "slug": genre.slug
-        } for genre in genre.parents],
-        "subgenres": get_genre_tree(genre)
-    }
+    return query
+
+def filter_genre_query(query, request):
+    """
+    Devuelve una consulta filtrada a partir de parametros entregados a través
+    de una URL.
+    """
+    if request.values.get("name") != None:
+        query = query.filter_by(name=request.values.get("name"))
+    
+    if request.values.get("top") != None:
+        if request.values.get("top") == 1:
+            query = query.filter_by(top=True)
+    
+    return query
