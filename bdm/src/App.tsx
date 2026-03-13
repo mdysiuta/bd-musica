57bbcf6df89c4c9982e244047cfe5f7a32998c65
mdysiuta <mdysiuta@gmail.com>
2026-03-13 20:47:58 -0300

    Estilización y cambio de apariencia rudimentaria

--- bdm/src/App.tsx
+++ bdm/src/App.tsx
@@ -5,7 +5,9 @@ import { releases } from './data'
 function App() {
     return (
         <>
-            <h1>Lista de lanzamientos</h1>
+            <h1 className='title'>Base de datos de música</h1>
+            <hr className='header-separator'/>
+            <h2 className='subtitle'>Lista de lanzamientos</h2>
             <ReleaseList releases={releases} />
         </>
     )
